5709e604be6d53d0e4d8905979085fa5edf84fd0
Kat <littlepawcoding@gmail.com>
2019-09-22 12:05:31 +0200

    Object's practice

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+// Practising Objects
+
+let pet = {
+  name: "Dug",
+  age: 1,
+  breed: "Cockerpoo"
+};
+
+pet.name = "Binki";
+
+pet["name"] = "Kanga";
+
+console.log(pet);
+console.log(pet.name);
