e7a143acdf5c6256ecabbff277f79819afa9261e
Kat <littlepawcoding@gmail.com>
2019-09-22 13:14:18 +0200

    Function practice

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-// Practising arrays
+// Practising functions
 
-let petList = ["Binki", "Kanga", "Dug"];
+// at this basic level there are two types of functions
+// one that performs a task
+// and one that calculates and returns a value
 
-// please list out all the item within the array called petList
-console.log(petList);
-// please pick out the index number 2 from the list of arrays and display in the console
-// remember, index starts from 0
-console.log(petList[2]);
+// this function is performing a task
+// please display the content on the console
 
-// binki = index 0
-// kanga = index 1
-// Dug = index 3
+function petName(dogName) {
+  console.log("Hello, my name is " + dogName + ". Woof!");
+}
 
-console.log(petList.length);
+petName("Dug");
+petName("Kanga");
+petName("Binki");
 
-// BUT there are 3 items within the array
+// this function is returning a value
+// please display the value of this action
+// add + add
 
-// please add another item to the array in index spot 3
-// in this case index 3 is empty, so it will just add at the end of the array
-// if the index was already occupied it would be replaced with a new value
-petList[3] = "TBC";
+function number(add) {
+  return add + add;
+}
 
-console.log(petList);
-console.log(petList[3]);
+let addNumber = number(3);
+console.log(addNumber);
+// 6
 
-petList[3] = "Simba";
+// please print the function number which contains the action add + add
+// the arguement that has been added is 5
+// so I'm asking for 5 + 5
+// this is also the shorter way of writing the above
 
-console.log(petList);
-console.log(petList[3]);
+console.log(number(5));
+// 10
 
-// arrays can also hold a mixture of values like strings and numbers, separately or together
-// this is called being dymanic
-// note that numbers do not need ""
-petList[4] = 7;
-
-console.log(petList);
-console.log(petList[4]);
-
-// arrays are just objects in Javascript!
-// usually objects are defined when use {}
-// and arrays are defined when use []
-// use typeof petList in the console and it'll tell you its an object
-// arrays are objects which hold collections of data
-// arrays are a data structure we use to store a list of items
+// there are two function calls above
+// number(5) is a function call
+// console.log() is a function call
