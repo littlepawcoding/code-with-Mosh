6459baef86f8f61aabb7cfae0ec1a1b48a9f79de
Kat <littlepawcoding@gmail.com>
2019-10-05 20:53:25 +0200

    operators quiz - blank

---
+++ index.js
@@ -0,0 +1,72 @@
+// Operators quiz:
+
+// 1- What is the result of “true && false”?
+// a- true
+// b- false
+// c- null
+// d- undefined
+
+// 2- What is the result of “(true && false) || true”?
+// a- true
+// b- false
+// c- null
+// d- undefined
+
+// 3- What is the value of y?
+// let x = 10;
+// let y = (x > 10) ? 1 : 0;
+// a- 10
+// b- 1
+// c- 0
+// d- 11
+
+// 4- What is the value of x?
+// let x = (2 + 3) * (4 + 5);
+// a- 45
+// b- 25
+// c- 20
+// d- 40
+
+// 5- Which expression returns true?
+// a- ‘1’ === 1;
+// b- 1 == 1
+// c- 1 === 1
+// d- b and c
+
+// 6- What is the value of x?
+// let x = (1 == true);
+// a- 1
+// b- true
+// c- false
+// d- undefined
+
+// 7- What is the value of y?
+// let x = 10;
+// let y = (x > 5) && (x < 15);
+// a- 10
+// b- 5
+// c- 15
+// d- true
+
+// 8- What is the value of x?
+// let x = 5; x += 3;
+// a- 3
+// b- 8
+// c- 15
+// d- 5
+
+// 9- What is the value of y?
+// let x = 10;
+// let y = x++;
+// a- 10
+// b- 11
+// c- 12
+// d- 13
+
+// 10- What is the value of y?
+// let x = 1;
+// let y = x !== 2;
+// a- 1
+// b- 2
+// c- false
+// d- true
