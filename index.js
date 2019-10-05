d95680416f5bdb6aad66c184069e282e4666be23
Kat <littlepawcoding@gmail.com>
2019-10-05 21:04:13 +0200

    Kat's answers to operators quiz

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-// Operators quiz:
+// Operators quiz: Kat's answers!
 
 // 1- What is the result of “true && false”?
 // a- true
+// b- false - ANSWER
+// c- null
+// d- undefined
+
+// true AND false - AND && both have to be true to return true.
+
+// 2- What is the result of “(true && false) || true”?
+// a- true - ANSWER
 // b- false
 // c- null
 // d- undefined
 
-// 2- What is the result of “(true && false) || true”?
-// a- true
-// b- false
-// c- null
-// d- undefined
+// (true AND false) OR true - false OR true - OR || at least one has to be true to return true
 
 // 3- What is the value of y?
 // let x = 10;
 // let y = (x > 10) ? 1 : 0;
 // a- 10
 // b- 1
-// c- 0
+// c- 0 - ANSWER
 // d- 11
+
+// if 10 is greater than 10 then return 1 otherwise return 0
+// 10 is not greater than 10 so return 0
+// if it was greater than or equal to >= the answer would be different
 
 // 4- What is the value of x?
 // let x = (2 + 3) * (4 + 5);
-// a- 45
+// a- 45 - ANSWER
 // b- 25
 // c- 20
 // d- 40
+
+// x = 5 * 9
+// x = 45
 
 // 5- Which expression returns true?
 // a- ‘1’ === 1;
 // b- 1 == 1
 // c- 1 === 1
-// d- b and c
+// d- b and c - ANSWER
 
 // 6- What is the value of x?
 // let x = (1 == true);
-// a- 1
+// a- 1 - ANSWER
 // b- true
 // c- false
 // d- undefined
@@ -46,20 +57,27 @@
 // a- 10
 // b- 5
 // c- 15
-// d- true
+// d- true - ANSWER
+
+// 10 > 5 // true
+// 10 < 15 // true
+// true AND true // true
 
 // 8- What is the value of x?
-// let x = 5; x += 3;
+// let x = 5;
+// x += 3;
 // a- 3
-// b- 8
+// b- 8 - ANSWER
 // c- 15
 // d- 5
+
+// ++ is plus one but += will be +3 in this case
 
 // 9- What is the value of y?
 // let x = 10;
 // let y = x++;
 // a- 10
-// b- 11
+// b- 11 - ANSWER
 // c- 12
 // d- 13
 
@@ -69,4 +87,6 @@
 // a- 1
 // b- 2
 // c- false
-// d- true
+// d- true - ANSWER
+
+// 1 is not equal to 2 // true
