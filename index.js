811685a79dea0f65a5fc7ac7199ac96939b8ceba
Kat <littlepawcoding@gmail.com>
2019-10-08 09:17:53 +0200

    Control flow (loops) exercise 1 - Kat's attempt

---
+++ index.js
@@ -0,0 +1,10 @@
+// Control flow exercises (loops)
+
+// 1) Write a function that takes two numbers and returns the maximum of the two.
+
+let x = 5;
+let y = 2;
+
+function maxNum(x, y) {
+  return x + y;
+}
