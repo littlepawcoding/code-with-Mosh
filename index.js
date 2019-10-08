14c4c9ab8125d9bdab853a03d21b8a8440e67eb4
Kat <littlepawcoding@gmail.com>
2019-10-08 09:30:04 +0200

    Exercise 1 - Kat's attempt 2 (didn't understand the question the first time)

--- index.js
+++ index.js
@@ -2,9 +2,28 @@
 
 // 1) Write a function that takes two numbers and returns the maximum of the two.
 
+// Kat's attempt:
 let x = 5;
 let y = 2;
 
 function maxNum(x, y) {
   return x + y;
 }
+
+// ah I fully didn't understand the question! I thought it was asking for the total of the two numbers and to return it, instead its asking for the largest of the two numbers.
+
+// Kat's attempt 2:
+
+let a = 77;
+let b = 34;
+
+function max(a, b) {
+  if (a > b) {
+    console.log(a);
+  } else console.log(b);
+}
+// if a, 28 is greater than b, 94 then print a
+// otherwise print b
+// hmm this is still not working
+
+// Solution:
